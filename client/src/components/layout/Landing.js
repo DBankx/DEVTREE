6184ac5927e765346dc37dfc11b47aaf8dc6f2ab
DBankx <61208493+DBankx@users.noreply.github.com>
2020-06-25 20:16:30 +0100

    remaking landing page ui

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import devtree from '../../img/devtree.png';
+import banner1 from '../../img/banner1.png';
 import Fab from '@material-ui/core/Fab';
 import Zoom from '@material-ui/core/Zoom';
 import { Link } from 'react-router-dom';
@@ -7,35 +8,31 @@ import { Link } from 'react-router-dom';
 const Landing = () => {
   return (
     <Fragment>
-      <div className='landing'>
-        <img className='logo-img' src={devtree} alt='' />
-        <div className='home'>
-          <div className='home-box'>
-            <h1>Welcome To DEVTREE</h1>
-            <p>
-              Connecting developers of different kinds or niche all around the
-              world
-            </p>
-            <ul className='btns-items'>
-              <li>
-                <Zoom in={true}>
-                  <Fab>
-                    <Link to='/register' className='btns-a'>
-                      Register
-                    </Link>
-                  </Fab>
-                </Zoom>
-              </li>
-              <li>
-                <Zoom in={true}>
-                  <Fab>
-                    <Link to='/login' className='btns-b'>
-                      Login
-                    </Link>
-                  </Fab>
-                </Zoom>
-              </li>
-            </ul>
+      <div className='landing-page'>
+        <div className='top-banner'>
+          <img className='logo-image' src={devtree} alt='app logo' />
+          <div className='top-content'>
+            <div className='top-content-words'>
+              <h1>Connecting Developers Wordlwide</h1>
+              <p>
+                Devtree is the number one platform for connecting with
+                developers from different niches world wide, and is trusted by
+                millions.
+              </p>
+              <ul>
+                <li>
+                  <Zoom in={true}>
+                    <Fab variant='extended'>Register</Fab>
+                  </Zoom>
+                </li>
+                <li>
+                  <Zoom in={true}>
+                    <Fab variant='extended'>Login</Fab>
+                  </Zoom>
+                </li>
+              </ul>
+            </div>
+            <img src={banner1} alt='Banner image' />
           </div>
         </div>
       </div>
