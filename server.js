3e7650c6d69e4bf1285d84cfef32805e91806459
DBankx <61208493+DBankx@users.noreply.github.com>
2020-07-01 17:34:58 +0100

    prepared for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 6000;
 const app = express();
 const connectToDB = require('./db');
 const cors = require('cors');
+const path = require('path');
 
 // connection to database
 connectToDB();
@@ -12,15 +13,21 @@ connectToDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('welcome to devtree');
-});
-
 // routes
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/authenticate'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/post'));
+
+// server static assests in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`your port has started on port ${port}`);
